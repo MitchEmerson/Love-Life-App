eb846bba1de40440a7d4871a776f83be61034917
Mitch Emerson <mitchemerson1984@hotmail.com>
2021-04-12 13:01:10 -0700

    pushing to get to last commit

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
-//console.log('this thing on?')
-/*Love-Life: At game start a user will push the "start button".  An input field will be available for users to insert the name of their love.  A timer will begin at the start of the game counting down from 5 minutes.  During the first two minutes, the user will see an image of "young love", during the third and fourth minute, the user will see an image of, "grown love", and the last minute will feature an image of, "old love".
-
-The game has metrics.  To keep their love alive, the user must press buttons. The buttons will add two points to the metrics of, "romance", "space", and "laughs".  The metrics will start at a 5, in a scale between 1-10.  Each of the metrics will decrease one point each second of the game.  If the user keeps all three metrics above 0 throughout the five minutes, they will receive an image of "strong love", and if they are unable to keep the metrics up...their love will die.  */
-
-    const loveLife = {
-    name: null,
-    age: "young love",
-    romance: 5,
-    space: 5,
-    laughs: 5,
-    progressBarsTimer: null,
-    time: 3,
-    //progressBarsTime: 5,  
-    
-    clickStart() {
-        $("#start-button").remove();
-        $("#romance-btn").on("click", loveLife.romanceHealth);
-        $("#space-btn").on("click", loveLife.spaceHealth);
-        $("#laughs-btn").on("click", loveLife.laughsHealth);
-        $("#countdown").on("click", loveLife.countdownEl);
-        loveLife.increaseAge();
-    
-    },
+function ageFunction() {
+    if(pet.age<100){
+        pet.age++;
+        $("#age").text(pet.age);
+    }
 
 
     
@@ -40,7 +21,7 @@ The game has metrics.  To keep their love alive, the user must press buttons. Th
             seconds = seconds < 10 ? 0 + seconds : seconds;
             //console.log(typeof seconds, "second log")
             
-            const $countdownEl = $("#countdown");
+            $countdownEl = $("#countdown");
             $countdownEl.text = `${minutes}: ${seconds}`;
             if (loveLife.time > 0) {
                 loveLife.time--;
@@ -62,16 +43,18 @@ The game has metrics.  To keep their love alive, the user must press buttons. Th
        
        romanceHealth() { 
            console.log(loveLife.romance);
-           
+           loveLife.romanceHealth++;
         },
         
         spaceHealth()  {
-            
+            console.log(loveLife.space);
+            loveLife.spaceHealth++;
         },
         
         
         laughsHealth() {
-            
+            console.log(loveLife.laughs);
+            loveLife.laughsHealth++;
             
         },
         
@@ -81,14 +64,8 @@ The game has metrics.  To keep their love alive, the user must press buttons. Th
     
     
     $("#start-button").on("click", loveLife.clickStart);
+    //$("countdownEl").on("click", loveLife.clickStart);
     
-    //$("#romance-btn").on("click", loveLife.romanceHealth);
-    
-    $("#space-btn").on("click", loveLife.clickStart);
-    
-    $("#laughs-btn").on("click", loveLife.clickStart);
-    
-    $("countdownEl").on("click", loveLife.clickStart);
     
     
     
@@ -104,22 +81,6 @@ The game has metrics.  To keep their love alive, the user must press buttons. Th
         }
             */
        
-    /*romanceHealth() { 
-         if ($('romance-btn').clicked == true)
-         console.log("am I stupid")
-         return loveLife.romance++;
-    }
-         
-    spaceHealth()  {
-        if($('space-btn').clicked == true)
-        return loveLife.space++;
-    }
-        
-    
-    laughsHealth() {
-        if($('laughs-btn').clicked == true)
-        return loveLife.laughs++;
-    };
     
     
     
