ac055f08b1793f2a3a0ea5ef29a7dfbd3d66109a
Mitch Emerson <mitchemerson1984@hotmail.com>
2021-04-12 04:40:32 -0700

    Added runaway timer

--- app.js
+++ app.js
@@ -3,28 +3,50 @@
 
 The game has metrics.  To keep their love alive, the user must press buttons. The buttons will add two points to the metrics of, "romance", "space", and "laughs".  The metrics will start at a 5, in a scale between 1-10.  Each of the metrics will decrease one point each second of the game.  If the user keeps all three metrics above 0 throughout the five minutes, they will receive an image of "strong love", and if they are unable to keep the metrics up...their love will die.  */
 
-const loveLife = {
+    const loveLife = {
+    name: null,
     age: "young love",
     romance: 5,
     space: 5,
     laughs: 5,
     progressBarsTimer: null,
-    //progressBarsTime: 5,
-    time: 60,  
-    timer: null,
-    start(event) {
-        console.log('Time for Love!');
+    //progressBarsTime: 5,  
+    
+    clickStart() {
+        
+        
         $("#start-button").remove();
-        $("#romance-btn").on("click", loveLife.romance.romanceHealth);
-        $("#space-btn").on("click", loveLife.space.spaceHealth);
-        $("#laughs-btn").on("click", loveLife.laughs.laughsHealth);
+        $("#romance-btn").on("click", loveLife.romanceHealth);
+        $("#space-btn").on("click", loveLife.spaceHealth);
+        $("#laughs-btn").on("click", loveLife.laughsHealth);
+        $("#countdown").on("click", loveLife.countdownEl);
     },
-    startTimer(){
-        loveLife.timer = setInterval(loveLife.reduceTime, 1000);
-        loveLife.progressBarsTimer = setInterval(loveLife.progressBarsReduceTime, 5000); 
-    },
-    romanceHealth() {
-        return loveLife.romance++;
+        
+}  
+        
+        
+        const startingTimer = 3;
+        let time = startingTimer * 60;
+
+        const countdownEl = document.getElementById('countdown');
+        
+        setInterval(updateCountdown, 1000);
+
+        function updateCountdown()  {
+            const minutes = Math.floor(time/60);
+            let seconds = time % 60;
+
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+
+            countdownEl.innerHTML = `${minutes}: ${seconds}`;
+            time--;
+        }
+    
+         
+    /*romanceHealth() {
+        console.log("this workin?")
+        loveLife.romance++;
+        
     },
     spaceHealth() {
         return loveLife.space++;
@@ -32,19 +54,17 @@ const loveLife = {
     laughsHealth() {
         return loveLife.laughs++;
     },
-    reduceTime(){
+    /*reduceTime(){
         loveLife.time--;
         if(loveLife.time <= 0){
             loveLife.age++;
             loveLife.time = 180;
             $("p").text(`Age of Love: ${loveLife.age}`);
         }
-    },
-
-};
-
-$("#start-button").on("click", loveLife.start); 
-
+    },*/
+ //
+/*
+$("#start-button").on("click", loveLife.start); */
 
 
 
@@ -63,7 +83,8 @@ $("#start-button").on("click", loveLife.start);
 
 
    
- 
+
+
 
 
 
